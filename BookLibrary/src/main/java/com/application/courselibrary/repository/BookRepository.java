99ad58d0a9d9b776406d390ede45cb40baca34c9
teo <teo.th@hotmail.gr>
2025-03-14 18:50:02 +0100

    dir clearing

--- BookLibrary/src/main/java/com/application/courselibrary/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.courselibrary.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.courselibrary.entity.Book;
-
-public interface BookRepository extends JpaRepository <Book, Long> {
-
-}
